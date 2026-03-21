f257f40704a0176114fa6bd1c73d1a60eb856e7d
Tanmay Singh <tanmaysinghx@gmail.com>
2026-03-21 19:08:31 +0530

    Fixes

--- application-pipelines/dev-builds/ts-api-engine-service-1606.groovy
+++ application-pipelines/dev-builds/ts-api-engine-service-1606.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
         APP_NAME = "ts-api-engine-service-1606"
         REGISTRY = "tanmaysinghx" // Your Docker Hub ID
-        DOCKERHUB_CREDS = credentials('DOCKERHUB_CREDS') 
+        DOCKERHUB_CREDS = credentials('dockerhub-creds') 
     }
 
     stages {
